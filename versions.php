7c8c37ce7bf9427d4f7848389b67f94c5a907306
Cees-Jan Kiewiet <ceesjank@gmail.com>
2023-09-03 13:19:33 +0200

    8.3.0beta3 => 8.3.0RC1

--- versions.php
+++ versions.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 $versions = [];
 
 if (getenv('INPUT_UPCOMINGRELEASES') == 'true') {
-    $versions[] = '8.3.0beta3';
+    $versions[] = '8.3.0RC1';
 }
 
 $d = new DOMDocument();
