2c8ed15c954860d3db631174c6236d0400c8cea2
Cees-Jan Kiewiet <ceesjank@gmail.com>
2025-07-29 22:44:06 +0200

    Add first 8.5: 8.5.0alpha2

--- versions.php
+++ versions.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 $versions = [];
 
 if (getenv('INPUT_UPCOMINGRELEASES') == 'true') {
-    //$versions[] = '8.5.0RC1';
+    $versions[] = '8.5.0alpha2';
 }
 
 $d = new DOMDocument();
