74c79002cdf245e2e86c9f05e7e8747568daf73a
Cees-Jan Kiewiet <ceesjank@gmail.com>
2025-09-02 17:15:37 +0200

    8.5.0alpha2 => 8.5.0beta2

--- versions.php
+++ versions.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 $versions = [];
 
 if (getenv('INPUT_UPCOMINGRELEASES') == 'true') {
-    $versions[] = '8.5.0alpha2';
+    $versions[] = '8.5.0beta2';
 }
 
 $d = new DOMDocument();
